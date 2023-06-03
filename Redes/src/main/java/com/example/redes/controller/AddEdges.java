17327cd6532a611deeb29f00965e134bd06745f5
Santiago Hernandez <125371075+juaca2oo4@users.noreply.github.com>
2023-06-02 21:00:25 -0500

    resolution

--- Redes/src/main/java/com/example/redes/controller/AddEdges.java
+++ Redes/src/main/java/com/example/redes/controller/AddEdges.java
@@ -34,57 +34,64 @@ public class AddEdges {
     void cancelA(ActionEvent event) {
         fromNodeTF.setText("");
         toNodeTF.setText("");
-        weightFT.setText("");
-        Stage currentStage = (Stage) weightFT.getScene().getWindow();
+        Stage currentStage = (Stage) toNodeTF.getScene().getWindow();
         currentStage.hide();
     }
 
     @FXML
     void continueA(ActionEvent event) {
-        Alert alert = new Alert(Alert.AlertType.ERROR);
-        double weight = 0;
-        String from="",to="";
-        if(fromNodeTF.getText().isEmpty()) {
-            alert.setTitle("Error!");
-            alert.setHeaderText("Please complete de field 'From node:'");
-            alert.showAndWait();
-        } else if(toNodeTF.getText().isEmpty()) {
-            alert.setTitle("Error!");
-            alert.setHeaderText("Please complete de field 'To node:'");
-            alert.showAndWait();
-        }else if(weightFT.getText().isEmpty()) {
-            alert.setTitle("Error!");
-            alert.setHeaderText("Please complete de field 'Edge Weight:'");
-            alert.showAndWait();
-        }else {
-            try {
-                weight = Double.parseDouble(weightFT.getText());
-            }catch (NumberFormatException er){
-                alert.setTitle("Error!");
-                alert.setHeaderText("Invalid input of 'Edge Weight:'");
-                alert.showAndWait();
-            }
-            try {
-                from=fromNodeTF.getText();
-            }catch (NumberFormatException er){
-                alert.setTitle("Error!");
-                alert.setHeaderText("Invalid input of 'From node:'");
-                alert.showAndWait();
-            }
-            try {
-                to=toNodeTF.getText();
-            }catch (NumberFormatException er){
-                alert.setTitle("Error!");
-                alert.setHeaderText("Invalid input of 'To node:'");
-                alert.showAndWait();
-            }
-            Vertex<String> v1= Graph.getInstance().findVertex(from);
-            Vertex<String> v2= Graph.getInstance().findVertex(to);
-            Graph.getInstance().addEdge(v1,v2,weight);
 
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        String from = fromNodeTF.getText();
+        String to = toNodeTF.getText();
+
+        if (from.isEmpty()) {
+            alert.setTitle("Error!");
+            alert.setHeaderText("Please complete the 'From node' field.");
+            alert.showAndWait();
+        } else if (to.isEmpty()) {
+            alert.setTitle("Error!");
+            alert.setHeaderText("Please complete the 'To node' field.");
+            alert.showAndWait();
+        } else {
+            Vertex<String> v1 = Graph.getInstance().findVertex(from);
+            Vertex<String> v2 = Graph.getInstance().findVertex(to);
+
+            if (v1 == null) {
+                alert.setTitle("Error!");
+                alert.setHeaderText("Invalid 'From node'. Node not found.");
+                alert.showAndWait();
+            } else if (v2 == null) {
+                alert.setTitle("Error!");
+                alert.setHeaderText("Invalid 'To node'. Node not found.");
+                alert.showAndWait();
+            } else {
+                double weight = calculateWeight(v1, v2);
+                Graph.getInstance().addEdge(v1, v2, weight);
+            }
         }
+
         fromNodeTF.setText("");
         toNodeTF.setText("");
-        weightFT.setText("");
+    }
+
+    private double calculateWeight(Vertex<String> v1, Vertex<String> v2) {
+        double lat1 = Math.toRadians(v1.getY()); // Coordenada Y representa la latitud
+        double lon1 = Math.toRadians(v1.getX()); // Coordenada X representa la longitud
+        double lat2 = Math.toRadians(v2.getY());
+        double lon2 = Math.toRadians(v2.getX());
+
+        double earthRadius = 6371; // Radio de la Tierra en kilómetros
+
+        // Fórmula de Haversine para calcular la distancia entre dos puntos en la Tierra
+        double dlon = lon2 - lon1;
+        double dlat = lat2 - lat1;
+        double a = Math.sin(dlat / 2) * Math.sin(dlat / 2)
+                + Math.cos(lat1) * Math.cos(lat2)
+                * Math.sin(dlon / 2) * Math.sin(dlon / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double distance = earthRadius * c;
+        System.out.println(distance);
+        return distance;
     }
 }
